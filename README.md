24cfddb183dd2ca84acda546b6d31733d97a33ce
qicfan <qicfan@gmail.com>
2024-12-04 15:43:13 +0800

    完成0.1.3版本
    增加alist支持，增加STRM删除，重构代码结构

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 ## 介绍
-#### 基于[p115client](https://github.com/ChenyangGao/p115client)开发，通过生成115目录树来快速完成STRM文件创建，由于只有一次请求所以不会触发风控，需要配合CD2挂载目录或者其他的挂载目录使用
-#### 不建议复制元数据，目前写死了1秒复制一个文件，加上网络传输，可能要好几秒才能处理一个文件，如果第一次扫盘有上万个文件可能要持续好几个小时。可以配合刮削器（如TMM）对生成的STRM文件再次刮削
+##### 基于[p115client](https://github.com/ChenyangGao/p115client)开发，通过生成115目录树来快速完成STRM文件创建，由于只有一次请求所以不会触发风控
+##### 不建议复制元数据，目前写死了1秒复制一个文件，加上网络传输，可能要好几秒才能处理一个文件，如果第一次扫盘有上万个文件可能要持续好几个小时。可以配合刮削器（如TMM）对生成的STRM文件再次刮削
 
+## 特性
+- 使用115目录树，只有一次请求，不会风控，后续操作都不会跟115交互
+- 支持两种使用模式
+    - 本地挂载目录：主要是CD2，会将媒体文件的真实路径写入STRM文件，支持复制元数据
+    - webdav：主要是alist，会讲媒体文件的webdav地址写入STRM文件，不支持复制元数据，如果alist存储选择了302，则支持直链播放
+- 每次执行时会对比115和本地文件，如果115不存在会删除对应的本地文件或目录，保持媒体库一致
+- 复制元数据时，每个文件间隔1秒，防止风控
+- 目录树文件在使用完后会自动删除（请不要中途中断执行）
 
 ## 一、准备工作
+### 获取Cookie
+需要通过各种方法获取cookie，然后写入./15-cookies.txt文件h或者写入config.json的115-cookies字段
+可以直接使用alist的115存储中添加的cookie
+
 ### 修改配置文件: config.json：
-- cloud_mount_root_dir: CD2里115网盘的挂载路径根目录， 例如：/CloudNAS/115，这个路径主要用来生成STRM文件的内容，以供媒体服务器可以正确播放。
+- 115-cookies: 将115的cookie放入这里，会优先使用115-cookies.txt文件
 - strm_root_dir: 115网盘挂载根目录对应的strm文件存放目录，例如：/vol2/1000/网盘/115，媒体服务器的媒体库就选择这个目录下的内容
 - strm_ext: 要生成strm文件的扩展名，默认包含音乐类型，如果只有电影，就手动删除对应扩展名如.wav、 .flac、 .mp3等
 - meta_ext: 元数据扩展名
-### 获取Cookie
-需要通过各种方法获取cookie，然后写入./15-cookies.txt文件中
+- local
+    - root_dir: 115网盘挂载的本地根目录，如果使用local模式，需要设置这个参数，例如：/CloudNAS/115
+- alist
+    - root_url: alist webdav的115根目录，例如：http://192.168.31.2:5244/dav/115
+    - username: alist用户名，例如：strm
+    - password: alist密码（不要包含:和@，尽量简单，可以专门添加一个只读权限的用户），例如：123123
 
 获取cookie的方法请自行搜索
 
@@ -19,7 +35,7 @@
 2. 打开终端切换到项目目录执行命令，比如解压到了D盘q115-strm目录：
 ```console
 cd D:\q115-strm
-main.exe -e=Media -c=0
+main.exe -t=local -e=Media -c=0
 ```
 
 ## 三、源码使用方法：
@@ -33,19 +49,26 @@ poetry install
 ```
 ### 执行脚本
 ```console
-python3 main.py -e=Media -c=0
+python3 main.py -t=local -e=Media -c=0
 ```
 
 ## 四、参数解释：
+-t 使用本地挂载目录或者alist，可选值：local, alist; 默认: local
+> - alist使用webdav生成strm内容，可以支持302直链播放，但是无法复制元数据
+> - local使用本地挂载目录（可以是CD2也可以是其他软件，只要能有本地真实目录即可），支持复制元数据
+
 -e 要生成目录树的目录，相对路径，不能为空，例如：
 - 单层：media
 - 多层：media/movie/合集
 
--c 是否复制元数据到strm目录：0-不复制，1-复制，元数据包括nfo，封面图片，字幕等，支持的扩展名在config.json的meta_ext修改
+-c 是否复制元数据到strm目录：0-不复制，1-复制; 默认：0
+> 元数据包括nfo，封面图片，字幕等，支持的扩展名在config.json的meta_ext修改
 
 ## 五、TODO
 - [x] STRM生成
 - [x] 元数据复制
+- [x] 支持源文件不存在时删除目标文件
+- [x] 支持alist webdav
 - [ ] docker支持 + 简单的web ui (v0.2版本)
 - [ ] docker版本增加监控文件变更，自动生成STRM (v0.2版本)
 - [ ] docker版本接入CD2的webhook，自动生成STRM (v0.2版本)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from p115client import P115Client, tool
-import argparse, os, json, logging, shutil, time
+import argparse, os, json, logging, shutil, time, sys
 from pathlib import Path
 
 _special_chars_map = {i: '\\' + chr(i) for i in b'()[]{}?*+|^$\\.'}
@@ -40,7 +40,8 @@ logger = logging.getLogger(__name__)
 
 config: dict | None = None
 export_dir_path: str = ''
-copy_meta_file: bool = True
+copy_meta_file: bool = False
+cloud_type: str = 'local'
 
 config_file = os.path.abspath("./config.json")
 with open(config_file, mode='r', encoding='utf-8') as fd_config:
@@ -48,88 +49,186 @@ with open(config_file, mode='r', encoding='utf-8') as fd_config:
 
 parser = argparse.ArgumentParser(prog='115-STRM', description='将挂载的115网盘目录生成STRM', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('-e', '--export_dir_path', help='要生成目录树的路径，相对路径，不能为空')
+parser.add_argument('-t', '--type', help='使用本地挂载目录或者alist，可选值：local, alist')
 parser.add_argument('-c', '--copy_meta_file', type=int, help='是否复制元数据，可选值：0, 1，0-不复制，1-复制')
 args, unknown = parser.parse_known_args()
 if args.copy_meta_file != None:
     copy_meta_file = args.copy_meta_file
 if args.export_dir_path != None:
     export_dir_path = args.export_dir_path
+if args.type != None:
+    cloud_type = args.type
+if cloud_type == 'local' and config['local']['root_dir'] == '':
+    logger.error('使用本地挂载模式时，必须配置local.root_dir，当前为空，清修改config.json')
+    sys.exit(1)
+if cloud_type == 'alist' and (config['alist']['root_url'] == '' or config['alist']['username'] == '' or config['alist']['password'] == ''):
+    logger.error('使用alist模式时，必须配置alist.root_url和用户名密码，当前为空，请修改config.json')
+    sys.exit(1)
 
-client = P115Client(Path("./115-cookies.txt").expanduser(), check_for_relogin=True)
+ed = export_dir_path.split('/')
+edr = os.sep.join(ed)
+dest_tree_list = []
 
 def work():
-    global config
-    global copy_meta_file
-    global export_dir_path
+    copy_list = []
+    src_tree_list = get_src_tree_list()
+    strm_base_dir = os.path.join(config['strm_root_dir'], edr)
+    dest_tree_list = get_dest_tree_list(config['strm_root_dir'], strm_base_dir, [edr])
+    added = []
+    for src_item in src_tree_list:
+        if src_item in dest_tree_list:
+            # 已存在，从dest中删除
+            dest_tree_list.remove(src_item)
+            continue
+        filename, ext = os.path.splitext(src_item)
+        if ext in config['strm_ext']:
+            strm_file = filename + '.strm'
+            if strm_file in dest_tree_list:
+                # 如果strm文件已存在，则从dest中删除
+                dest_tree_list.remove(strm_file)
+                continue
+            else:
+                added.append(src_item)
+                continue
+        if ext in config['meta_ext']:
+            # 如果是元数据，则加入复制列表
+            copy_list.append(src_item)
+        
+    # added是要添加的, dest_tree_list剩下的是要删除的， copy_list是要复制的元数据
+    # 处理删除
+    c = 0
+    dt = len(dest_tree_list)
+    ds = 0
+    df = 0
+    for delete_item in dest_tree_list:
+        c += 1
+        delete_real_dir = os.path.join(config['strm_root_dir'], delete_item)
+        if os.path.exists(delete_real_dir):
+            try:
+                os.remove(delete_real_dir)
+                logger.info('[%d / %d] 删除：%s' % (c, dt, delete_item))
+                ds += 1
+            except OSError as e:
+                logger.error('[%d / %d] 错误：%s \n %s' % (c, dt, delete_item, e))
+                df += 1
+    # 处理添加
+    c = 0
+    at = len(added)
+    asuc = 0
+    af = 0
+    for item in added:
+        c += 1
+        rs = strm(item)
+        if rs == '':
+            # 成功
+            asuc += 1
+            logger.info('[%d / %d] STRM：%s' % (c, at, item))
+        else:
+            af += 1
+            logger.error('[%d / %d] 错误：%s \n %s' % (c, at, item, e))
+    # 处理元数据
+    if cloud_type == 'alist':
+        logger.info('alist不处理元数据')
+    else:
+        if copy_meta_file == True :
+            c = 0
+            ct = len(copy_list)
+            cs = 0
+            cf = 0
+            for item in copy_list:
+                c += 1
+                src_file = os.path.join(config['local']['root_dir'], item)
+                dest_file = os.path.join(config['strm_root_dir'], item)
+                if not os.path.exists(src_file):
+                    cf += 1
+                    logger.error('[%d / %d] 元数据 - 源文件不存在：%s' % (c, ct, src_file))
+                    continue
+                try:
+                    logger.info('[%d / %d] 元数据 - 复制：%s' % (c, ct, item))
+                    shutil.copy(src_file, dest_file)
+                    cs += 1
+                except OSError as e:
+                    logger.error('[%d / %d] 元数据 - 复制错误：%s \n %s' % (c, ct, item, e))
+                    cf += 1
+                time.sleep(1)
+            logger.info('元数据结果：成功: {0}, 失败: {1}, 总共: {2}'.format(cs, cf, ct))
+    logger.info('删除结果：成功: {0}, 失败: {1}, 总共: {2}'.format(ds, df, dt))
+    logger.info('STRM结果：成功: {0}, 失败: {1}, 总共: {2}'.format(asuc, af, at))
+    
+
+def get_src_tree_list():
+    ### 解析115目录树，生成目录数组
+    cookies = ''
+    if os.path.exists('./115-cookies.txt'):
+        with open('./115-cookies.txt', mode='r', encoding='utf-8') as f:
+            cookies = f.read()
+    if cookies == '' and config['115-cookies'] != '':
+        cookies = config['115-cookies']
+    if cookies == '':
+        logger.error('请先配置155 cookies')
+        sys.exit(1)
+    client = P115Client(Path("../strm/115-cookies.txt").expanduser(), check_for_relogin=True)
     it = tool.export_dir_parse_iter(client=client, export_file_ids=export_dir_path, target_pid=export_dir_path, parse_iter=tool.parse_export_dir_as_dict_iter, 
                            delete=True, async_=False, show_clock=True)
     i = 0
     path_index = {}
-    copy_list = []
-    ed = export_dir_path.split('/')
-    edr = os.sep.join(ed)
-    cloud_base_dir = os.path.join(config['cloud_mount_root_dir'], edr)
-    strm_base_dir = os.path.join(config['strm_root_dir'], edr)
-    if not os.path.exists(strm_base_dir): 
-        os.makedirs(strm_base_dir)
+    src_tree_list = []
     for item in it:
         i += 1
         parent = path_index.get(item['parent_key'])
         if parent is None:
             item['path'] = ''
         else:
-            if i == 2 and cloud_base_dir.endswith(item['name']):
-                item['path'] = ''
+            if i == 2 and edr.endswith(item['name']):
+                item['path'] = edr
             else:
                 item['path'] = os.path.join(parent['path'], item['name'])
-        item['create'] = False
         path_index[item['key']] = item
-        if i == 1 or parent['name'] == '':
-            continue
-        if parent['create'] is False:
-            # 处理父级，创建目录
-            parent['create'] = True
-            parent_dest_dir = os.path.join(strm_base_dir, parent['path'])
-            if os.path.exists(parent_dest_dir):
-                logger.info('目录 - 存在：' + parent_dest_dir)
-            else:
-                os.mkdir(parent_dest_dir)
-                logger.info('目录 - 创建：' + parent_dest_dir)       
-        filename, ext = os.path.splitext(item['path'])
-        if ext in config['strm_ext']:
-            # 需要生成strm
-            try:
-                content = os.path.join(cloud_base_dir, item['path'])
-                strm_file = os.path.join(strm_base_dir, filename + '.strm')
-                if os.path.exists(strm_file):
-                    logger.info('文件 - 存在：' + strm_file)
-                else:
-                    with open(strm_file, 'w', encoding='utf-8') as f:
-                        f.write(content)
-                    logger.info('文件 - 生成strm：' + strm_file)
-            except OSError as e:
-                logger.error('文件 - 生成strm：{0} \n {1}'.format(strm_file, e))
-        if ext in config['meta_ext']:
-            copy_list.append(item['path'])
-    c = 0
-    t = len(copy_list)
-    for copy_item in copy_list:
-        c += 1
-        src_file = os.path.join(cloud_base_dir, copy_item)
-        dest_file = os.path.join(strm_base_dir, copy_item)
-        if not os.path.exists(src_file):
-            logger.error('[%d / %d] 元数据 - 源文件不存在：%s' % (c, t, src_file))
-            continue
-        if os.path.exists(dest_file):
-            logger.info('[%d / %d] 元数据 - 存在：%s' % (c, t, copy_item))
-            continue
-        try:
-            logger.info('[%d / %d] 元数据 - 复制：%s' % (c, t, copy_item))
-            shutil.copy(src_file, dest_file)
-        except OSError as e:
-            logger.error('[%d / %d] 元数据 - 复制错误：%s \n %s' % (c, t, copy_item, e))
-        
-        time.sleep(1)
+        if item['path'] != '':
+            src_tree_list.append(item['path'])
+    return src_tree_list
 
+def get_dest_tree_list(base_dir: str, root_dir: str, dest_tree_list: list):
+    ### 获取目标路径目录树，用于处理差异
+    if not os.path.exists(root_dir):
+        return dest_tree_list
+    dirs = os.listdir(root_dir)
+    for dir in dirs:
+        if dir == '.' or dir == '..': continue
+        item = os.path.join(root_dir, dir)
+        dest_tree_list.append(item.replace(base_dir + os.sep, ''))
+        if os.path.isfile(item):
+            # 如果是文件，则不用递归
+            continue
+        else:
+            get_dest_tree_list(base_dir, item, dest_tree_list)
+    return dest_tree_list
+
+def strm(path: str):
+    try:
+        dirname = os.path.dirname(path)
+        real_dirname = os.path.join(config['strm_root_dir'], dirname)
+        if not os.path.exists(real_dirname):
+            os.makedirs(real_dirname)
+        filename, ext = os.path.splitext(path)
+        # 生成STRM文件
+        strm_file = filename + '.strm'
+        strm_real_file = os.path.join(config['strm_root_dir'], strm_file)
+        strm_content = ''
+        if type == 'local':
+            strm_content = os.path.join(config['local']['root_dir'], path)
+        else:
+            url = config['alist']['root_url'].replace('http://', '')
+            strm_content = 'http://{0}:{1}@{2}/{3}'.format(config['alist']['username'], config['alist']['password'], url, path.replace(os.sep, '/'))
+        with open(strm_real_file, 'w', encoding='utf-8') as f:
+            f.write(strm_content)
+        return ''
+    except OSError as e:
+        return e
+    
 if __name__ == '__main__':
+    bt = time.time()
     work()
+    et = time.time()
+    tt = (et-bt) / 1000
+    logger.info('执行完成，总耗时：{:.3f}秒'.format(tt))
