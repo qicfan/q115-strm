88e1c0a831a92646603f407df1de69704728e2c8
qicfan <qicfan@gmail.com>
2024-12-14 03:13:26 +0800

    修正监控的地址，允许容器外访问

--- server.py
+++ server.py
@@ -168,4 +168,4 @@ if __name__ == '__main__':
     LIBS.initCron()
     watchProcess = Process(target=StartWatch)
     watchProcess.start()
-    app.run(port=12123)
+    app.run(host="0.0.0.0", port=12123)
