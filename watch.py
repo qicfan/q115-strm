80765b57bdb432538ec004cbaba428a5042ba6b6
qicfan <qicfan@gmail.com>
2024-12-20 02:01:18 +0800

    v0.3.3.5
    修正监控变更其他网盘时生成路径错误的问题

--- watch.py
+++ watch.py
@@ -25,7 +25,10 @@ class FileEventHandler(FileSystemEventHandler):
     
     def getStrmPath(self, path):
         # 返回目标位置路径
-        newPath: str = path.replace(self.lib.path_of_115, '')
+        if self.lib.cloud_type == '115':
+            newPath: str = path.replace(self.lib.path_of_115, '')
+        else:
+            newPath: str = path.replace(self.path, '')
         newPath = newPath.lstrip(os.sep)
         return os.path.join(self.lib.strm_root_path, newPath)
 
